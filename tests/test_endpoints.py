9a8a251faee6fa45f62fb43e6901e0fef1c4094b
Brennan Vincello <brennanv03@gmail.com>
2017-10-05 07:35:21 -0700

    refactor scripts

--- tests/test_endpoints.py
+++
@@ -1,9 +0,0 @@
-# test endpoints
-# curl http://localhost:8080/buildings/16/19293/24641.json
-# curl http://localhost:8080/buildings/16/19293/24641.mvt
-# curl http://localhost:8080/all/16/19293/24641.json
-
-
-def test_endpoints():
-    # TODO write tests
-    assert False
